788f90cbf3c05e9fcb78bff59108c248895e73d1
Pusca Vasile <vasea-pusca@mail.ru>
2025-11-29 10:49:21 +0200

    Add pojo class coffe

--- src/Coffee.java
+++ src/Coffee.java
@@ -1,2 +1,29 @@
 public class Coffee {
+
+    //properties
+    private int temp;
+    private int conc;
+
+    //constructor
+
+    public Coffee(int temp, int conc){
+        this.temp = temp;
+        this.conc = conc;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public int getConc() {
+        return conc;
+    }
+
+    @Override
+    public String toString() {
+        return "[temperatura Coffee = " +
+                 + temp +
+                ": concentratia =" + conc +
+                ']';
+    }
 }
