7b52f131df7a1c4af9462ef4ddbace49f766e9fe
Pusca Vasile <vasea-pusca@mail.ru>
2025-11-29 10:55:41 +0200

    Add CoffeeMaker method

--- src/CoffeeMaker.java
+++ src/CoffeeMaker.java
@@ -1,2 +1,10 @@
 public class CoffeeMaker {
+
+    Coffee makeCoffee() {
+        System.out.println("Coffee is preparing ...");
+        int t = (int) (Math.random() * 100);
+        int c = (int) (Math.random() * 100);
+        Coffee coffee = new Coffee(t,c);
+        return coffee;
+    }
 }
