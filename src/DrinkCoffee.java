8bc9886e4f5f228d9834a8b0d8f19daff5445b9d
Pusca Vasile <vasea-pusca@mail.ru>
2025-11-29 11:23:12 +0200

    throw temperaturException

--- src/DrinkCoffee.java
+++ src/DrinkCoffee.java
@@ -1,7 +1,13 @@
-//public class DrinkCoffee {
-//
-//    void drinkCoffee(Coffee coffee){
-//
-//        if (coffee.getTemp())
-//    }
-//}
+public class DrinkCoffee {
+
+    void drinkCoffee(Coffee coffee){
+
+        if (coffee.getTemp() > 65) {
+            throw new TemperaturaException(coffee.getTemp(), "Cofeaua e prea fierbinte. O aruncam!")
+        }
+
+        if (coffee.getConc() > 55){
+            throw new ConcentrationException(coffee.getConc(), "concentratia cafeleie e prea mare. O aruncam!")
+        }
+    }
+}
